6542c9efe091b59a9dde5ca722a3fded39635a1c
unknown <mdamir4298@gmail.com>
2023-12-03 13:11:10 +0530

    gem garden backend done

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -4,20 +4,15 @@ const { productModel } = require("../models/product.model");
 const productsController = express.Router();
 
 // get all products data using get method
-productsController.get("/read", async(req,res)=>{
-  try{
-    const products= await productModel.find();
-    res.status(200).json({status:"all products",data:products})
-
+productsController.get("/read", async (req, res) => {
+  try {
+    const products = await productModel.find();
+    res.status(200).json({ status: "all products", data: products });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
   }
-  catch(err){
-    console.log(err)
-    res.send(err)
-  }
- 
-})
-
-
+});
 
 // Getting data by category
 productsController.get("/:category", async (req, res) => {
@@ -38,10 +33,10 @@ productsController.get("/:category", async (req, res) => {
   }
 });
 
-
 // here are all functionalities implemented on this route "/"
 productsController.get("/", async (req, res) => {
-  let { category, sortBy, order, pageSize, limit, brand, title, filter, q } = req.query;
+  let { category, sortBy, order, pageSize, limit, brand, title, filter, q } =
+    req.query;
 
   // getting data by search
   const search = q;
@@ -59,9 +54,7 @@ productsController.get("/", async (req, res) => {
       ],
     });
 
-    res
-      .status(200)
-      .json({ status: "All your searched data", data: productData });
+    res.status(200).json({ status: "All searched data", data: productData });
     console.log(productData);
   }
 
@@ -76,28 +69,31 @@ productsController.get("/", async (req, res) => {
       .sort({ [sortBy]: order === "asc" ? 1 : -1 });
 
     res.json({ status: "here is limited products on page", data: product });
-
-  }else if (pageSize && limit) {  //   pagination with limit
-    const products = await productModel.find().skip(skipDatas).limit(limit)
-    res.json({status:"success",data:products})
-
-  }else if(category && sortBy && order){ 
-   const products= await productModel.find({category:category}).sort({[sortBy]:order === "asc" ? 1:-1})
-   console.log(products)
-   console.log(req.query)
-   res.json({status:"sorted data by category",data:products})
-
-  }else if(title && sortBy && order){
-    const products= await productModel.find({title:title}).sort({[sortBy]:order === "asc"?1:-1})
-    res.json({status:"sorted data by title",data:products})
-
-  }else if (brand && order && sortBy) {
+  } else if (pageSize && limit) {
+    //   pagination with limit
+    const products = await productModel.find().skip(skipDatas).limit(limit);
+    res.json({ status: "success", data: products });
+  } 
+  else if (title && sortBy && order) {
+    const products = await productModel
+      .find({ title: title })
+      .sort({ [sortBy]: order === "asc" ? 1 : -1 });
+    res.json({ status: "sorted data by title", data: products });
+  } 
+  else if (category && sortBy && order) {
+    const products = await productModel
+      .find({ category: category })
+      .sort({ [sortBy]: order === "asc" ? 1 : -1 });
+    console.log(products);
+    console.log(req.query);
+    res.json({ status: "sorted data by category", data: products });
+  } else if (brand && order && sortBy) {
     if (order === "asc") {
-    const  products = await productModel.find({ brand: brand }).sort({
+      const products = await productModel.find({ brand: brand }).sort({
         [sortBy]: 1,
       });
       return res.json({ status: "success", data: products });
-    }else if(order === "desc"){
+    } else if (order === "desc") {
       products = await productModel.find({ brand: brand }).sort({
         [sortBy]: -1,
       });
@@ -105,30 +101,33 @@ productsController.get("/", async (req, res) => {
     }
   }
 
+   // Sort by price in ascending order
   if (sortBy === "price" && order === "asc") {
-   const  products = await productModel.find().sort({ price: 1 });
+    const products = await productModel.find().sort({ price: 1 });
     return res.json({ products });
-  } else if (sortBy === "price" && order === "desc") {
+  } 
+   // Sort by price in descending order
+  else if (sortBy === "price" && order === "desc") {
     products = await productModel.find().sort({ price: -1 });
     return res.json({ products });
-  } else if (category) {
-   const  products = await productModel.find({ category: category });
+  } 
+  else if (category) {
+    const products = await productModel.find({ category: category });
     return res.json({ status: "sorted by category products", data: products });
-  } else if (brand) {
-   const  products = await productModel.find({ brand: brand });
+  } 
+  else if (brand) {
+    const products = await productModel.find({ brand: brand });
     return res.json({ status: "sorted by brand products", data: products });
-  } else if (title) {
-   const  products = await productModel.find({ title: title });
+  }
+   else if (title) {
+    const products = await productModel.find({ title: title });
     return res.json({ status: "sorted by title products", data: products });
-  } else {
-   const products = await productModel.find();
+  }
+   else {
+    const products = await productModel.find();
     return res.json({ status: "All products", data: products });
   }
-
 });
 
-
-
-
-
+// Export the productsController for use in other parts of the application
 module.exports = { productsController };
--- routes/userPersonalInfo.routes.js
+++ routes/userPersonalInfo.routes.js
@@ -1,59 +1,82 @@
 const express = require("express");
-const {userInfoModel} = require("../models/user.model")
-require("dotenv").config()
+const { userInfoModel } = require("../models/user.model");
+require("dotenv").config();
 
+const userInfoController = express.Router();
 
-const userInfoController = express.Router()
+// Get user information by user ID using GET method
+userInfoController.get("/", async (req, res) => {
+  const { userId } = req.body;
 
-userInfoController.get("/", async(req,res)=>{
-    const {userId} = req.body;
+   // Retrieve user information based on the provided user ID
+  const userInfo = await userInfoModel.find({ userId: userId });
+  // Respond with user data in JSON format
+  res.status(200).json({ userdata: userInfo });
+});
 
-    const userInfo= await userInfoModel.find({userId: userId})
-    res.status(200).json({userdata: userInfo})
+// Create a new user information entry using POST method
+userInfoController.post("/create", async (req, res) => {
+  const { firstName, lastName, email, phone, city, state, pincode, userId } =
+    req.body;
 
-    
-})
+     // Create a new user information object
+  const userinfoAdded = new userInfoModel({
+    firstName,
+    lastName,
+    email,
+    phone,
+    city,
+    state,
+    pincode,
+    userId,
+  });
 
+  try {
+    // Save the new user information entry to the database
+    await userinfoAdded.save();
 
-userInfoController.post("/create", async(req,res)=>{
-    const {firstName,lastName,email,phone,city,state,pincode,userId} = req.body;
+    // Respond with a success message and the created user data
+    res.status(200).json({ message: "created", createdData: userinfoAdded });
+  } catch (err) {
+    // Handle errors and respond with an error message
+    console.log(err);
+    res.send("something went wrong");
+  }
+});
 
-    const userinfoAdded = new userInfoModel({firstName,lastName,email,phone,city,state,pincode,userId})
+// Delete user information by ID using DELETE method
+userInfoController.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
 
-    try{
-        await userinfoAdded.save()
-        res.status(200).json({message:"created",createdData:userinfoAdded})
+   // Attempt to find and delete user information based on the provided ID and user ID
+  const deletedData = await userInfoModel.findOneAndDelete({
+    _id: id,
+    userId: req.body.userId,
+  });
+  // Respond with a success message and the deleted user data, or an error message if deletion fails
+  if (deletedData) {
+    res.status(200).json({ message: "deleted", data: deletedData });
+  } else {
+    res.status(400).json("couldn't delete");
+  }
+});
 
-    }
-    catch(err){
-        console.log(err)
-        res.send("something went wrong")
-    }
-})
+// Edit user information by ID using PATCH method
+userInfoController.patch("/edit/:id", async (req, res) => {
+  const { id } = req.params;
 
+   // Attempt to find and update user information based on the provided ID and user ID
+  const editUserInfo = await userInfoModel.findOneAndUpdate(
+    { _id: id, userId: req.body.userId },
+    { ...req.body }
+  );
+  // Respond with a success message and the edited user data, or an error message if editing fails
+  if (editUserInfo) {
+    res.status(200).json({ message: "edited", data: editUserInfo });
+  } else {
+    res.status(400).json("couldn't edit");
+  }
+});
 
-userInfoController.delete("/delete/:id", async(req,res)=>{
-    const {id} = req.params;
-    const {userId} = req.body;
-    const deletedData = await userInfoModel.findOneAndDelete({_id:id, userId:userId})
-    if(deletedData){
-        res.send("deleted", deletedData)
-    }else{
-        res.send("couldn't delete")
-    }
-    
-})
-
-
-userInfoController.patch("/edit/:id", async(req,res)=>{
-    const {id} =req.params;
-
-    const editUserInfo = await userInfoModel.findOneAndUpdate({_id: id , userId:req.body.userId}, ...req.body)
-    if(editUserInfo){
-        res.send("edited", editUserInfo)
-    }else{
-        res.send("couldn't edit")
-    }
-
-})
-module.exports = {userInfoController}
+// Export the userInfoController for use in other parts of the application
+module.exports = { userInfoController };
